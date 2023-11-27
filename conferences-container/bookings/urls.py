3c6175830d99529bea7f3106469fa6ec0c3b2f2a
MathewSchofield <mathewschofield@hotmail.com>
2023-11-27 12:51:07 +0000

    added views and urls

--- conferences-container/bookings/urls.py
+++ conferences-container/bookings/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="index"),  # e.g. http://localhost:8080/
+    path("new/", views.new, name="new"),  # e.g. http://localhost:8080/new/
+    path("new/<int:booking_id>/step2/", views.add_church, name="add_church"),
+    path("new/<int:booking_id>/confirm/", views.confirm, name="confirm"),
+    path("new/<int:booking_id>/complete/", views.complete, name="complete"),
 ]
--- conferences-container/bookings/views.py
+++ conferences-container/bookings/views.py
@@ -2,3 +2,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world!")
+
+def new(request):
+    return HttpResponse("new. for putting in details")
+
+def add_church(request, booking_id):
+    return HttpResponse("step 2. for adding church to %s." % booking_id)
+
+def confirm(request, booking_id):
+    return HttpResponse("show all data + confirms. completion flag set to true")
+
+def complete(request, booking_id):
+    return HttpResponse("Completion")
