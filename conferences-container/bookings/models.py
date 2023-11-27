8c9edb25d250739a2a9411bcffa9203dde1385f9
MathewSchofield <mathewschofield@hotmail.com>
2023-11-27 11:57:37 +0000

    Step 3 - initial Db migrations and models

--- conferences-container/bookings/models.py
+++ conferences-container/bookings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Bookings(models.Model):
+    email = models.EmailField()
+    title = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    home_church = models.CharField(max_length=200)
+    completed = models.BooleanField()
