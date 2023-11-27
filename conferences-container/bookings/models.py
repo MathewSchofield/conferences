a024402b504bb9de89505ca17aa4b0f656601d6b
MathewSchofield <mathewschofield@hotmail.com>
2023-11-27 12:21:16 +0000

    migrate bookings model

--- conferences-container/bookings/models.py
+++ conferences-container/bookings/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Bookings(models.Model):
+
+    #def __str__(self):
+    #    return self.question_text
+
     email = models.EmailField()
     title = models.CharField(max_length=100)
     first_name = models.CharField(max_length=200)
