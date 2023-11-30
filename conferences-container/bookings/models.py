f73caba884a075cc092f266dad9aeb29fbcaf35d
MathewSchofield <mathewschofield@hotmail.com>
2023-11-30 17:20:21 +0000

    finished step 5

--- conferences-container/bookings/models.py
+++ conferences-container/bookings/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
+
+TITLE_CHOICES = (
+    (None, ""),  # By default, no title choice is selected
+    ('Mr', 'Mr'),
+    ('Mrs', 'Mrs'),
+    ('Ms', 'Ms'),
+    ('Miss', 'Miss'),
+)
+
+
 class Booking(models.Model):
 
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=4, choices=TITLE_CHOICES, default="")
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField()
     home_church = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
-
-
-
-    #def __str__(self):
-    #   return self.email
