a51a6a850c4289469645a4c12b7fa76abb759fb7
MathewSchofield <mathewschofield@hotmail.com>
2023-12-01 16:19:51 +0000

    test work

--- conferences-container/bookings/tests.py
+++ conferences-container/bookings/tests.py
@@ -10,12 +10,24 @@ def create_booking():
 
 
 class BookingNewViewTests(TestCase):
+    def test_new111(self):
+        """ Test the new View. Does not require any parameters. """
+        response = self.client.post(reverse("new"))
+        self.assertEqual(response.status_code, 200)
+        self.assert
+
     def test_new(self):
         """ Test the new View. Does not require any parameters. """
         response = self.client.get(reverse("new"))
         self.assertEqual(response.status_code, 200)
 
 
+
+
+
+
+
+# ADD MULTIPLE PARAMETERS TO TESTS (add_church, confirm, complete) ######################
 class BookingAddChurchViewTests(TestCase):
     """ Test the add_church View. Requires a valid Booking id. """
 
